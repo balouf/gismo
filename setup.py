30bbb68dc3f84b1d83ff97b34a49ada31637af54
Fabien <fabien.mathieu@normalesup.org>
2020-05-04 22:05:25 +0200

    Add acm.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['scipy', 'scikit-learn', 'dill', 'numpy', 'numba']
+requirements = ['scipy', 'scikit-learn', 'dill', 'numpy', 'numba', 'beautifulsoup4',
+                'lxml', 'xmltodict']
 
 setup_requirements = ['pytest-runner', ]
 
