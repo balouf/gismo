8fb9476fc03b9835d552e2d436777ce2b0d19dc0
Fabien <fabien.mathieu@normalesup.org>
2020-05-05 20:16:13 +0200

    Remove useless xmltodict package from setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['scipy', 'scikit-learn', 'dill', 'numpy', 'numba', 'beautifulsoup4',
-                'lxml', 'xmltodict']
+                'lxml']
 
 setup_requirements = ['pytest-runner', ]
 
