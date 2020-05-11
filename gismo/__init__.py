785aac3f0eb688d3d733fd62a9bee7e713142120
Fabien <fabien.mathieu@normalesup.org>
2020-05-11 12:39:04 +0200

    Bump version: 0.2.4 → 0.2.5

--- gismo/__init__.py
+++ gismo/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fabien Mathieu"""
 __email__ = 'fabien.mathieu@normalesup.org'
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 from gismo.corpus import Corpus
 from gismo.embedding import Embedding
